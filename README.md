6cdc656741a5327ed10adf286d4416631e92ecee
Pandademic <atharv.gupte510@gmail.com>
2021-12-16 20:27:07 -0500

    add all files

---
+++ README.md
@@ -0,0 +1,5 @@
+# object-detection
+_______
+CocoSsd (Coco Single Shot MultiBox Detection) 
+This model detects objects defined in the COCO dataset, which has a large-set of objects for object detection. 
+After CocoSsd model has detected a image, it returns - - label for the image - objects accuracy/confidence - width and height of the object - x and y coordinates of the object - meaning where is the object located on the image
---
+++ main.js
@@ -0,0 +1,41 @@
+img = "";
+status = "" ;
+objects=[];
+//dumy commit to change SHA256 file hash
+function preload(){
+    img = loadImage('dog-cat.jpg');
+}
+function setup() { 
+    canvas = createCanvas(640, 420);
+    canvas.center();
+    objectDetector=ml5.objectDetector('cocossd',modelLoaded); 
+    document.getElementById('status').innerHTML = "Status : Detecting Objects";
+}
+function draw(){
+    image(img,0,0,640,420);//cover the canvas with image
+    if(status!=""){
+        for(i=0;i<objects.length;i++){
+           document.getElementById("status").innerHTML="status: object Detected";
+           fill("#FF0000");
+           percent=floor(objects[i].confidence*100);
+           text(object[i].label+" " + percent + "%",objects[i].x,objects[i].y);
+           noFill();
+           stroke("#FF0000");
+           rect(objects[i].x,object[i].y,objects[i].width,objects[i].height); 
+        }
+    }
+}
+function modelLoaded(){
+    console.log("Coco has arrived.Bus 165 with SS is running 30 minutes late! D is still asleep and may not arrive at all ! ");
+    status=true;
+    objectDetector.detect('img',gotResult);
+}
+function gotResult(error,results){
+    if(error){
+        console.error(error);
+    }
+    else{
+        console.log(results);
+        objects=results;
+    }
+}
