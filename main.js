2b4c18ce9fa61070073a4c30a4265d1fdbc5d34c
Pandademic <74566464+Pandademic@users.noreply.github.com>
2021-12-16 20:40:51 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,42 +1,100 @@
+objectDetector= "";
+
 img = "";
-status = "" ;
-objects=[];
-objectDetector="";
-//dumy commit to change SHA256 file hash
+objects = [];
+status = "";
+
 function preload(){
-    img = loadImage('dog-cat.jpg');
+  img = loadImage('dog_cat.jpg');
 }
-function setup() { 
-    canvas = createCanvas(640, 420);
-    canvas.center();
-    objectDetector=ml5.objectDetector('cocossd',modelLoaded); 
-    document.getElementById('status').innerHTML = "Status : Detecting Objects";
+
+
+function setup() {
+  canvas = createCanvas(640, 420);
+  canvas.center();
+  objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+  document.getElementById("status").innerHTML = "Status : Detecting Objects";
 }
-function draw(){
-    image(img,0,0,640,420);//cover the canvas with image
-    if(status!=""){
-        for(i=0;i<objects.length;i++){
-           document.getElementById("status").innerHTML="status: object Detected";
-           fill("#FF0000");
-           percent=floor(objects[i].confidence*100);
-           text(object[i].label+" " + percent + "%",objects[i].x,objects[i].y);
-           noFill();
-           stroke("#FF0000");
-           rect(objects[i].x,object[i].y,objects[i].width,objects[i].height); 
+
+function modelLoaded() {
+    objectDetector= "";
+
+img = "";
+objects = [];
+status = "";
+
+function preload(){
+  img = loadImage('dog_cat.jpg');
+}
+
+
+function setup() {
+  canvas = createCanvas(640, 420);
+  canvas.center();
+  objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+  document.getElementById("status").innerHTML = "Status : Detecting Objects";
+}
+
+function modelLoaded() {
+  console.log("Coco has arrived.Bus 165 with SS is running 30 minutes late! D is still asleep and may not arrive at all ! ");
+  status = true;
+  objectDetector.detect(img, gotResult);
+}
+
+function gotResult(error, results) {
+  if (error) {
+    console.log(error);
+  }
+  console.log(results);
+  objects = results;
+}
+
+
+function draw() {
+  image(img, 0, 0, 640, 420);
+
+      if(status != "")
+      {
+        for (var i = 0; i < objects.length; i++) {
+          document.getElementById("status").innerHTML = "Status : Object Detected";
+    
+          fill(255, 0, 0);
+          percent = floor(objects[i].confidence * 100);
+          text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15);
+          noFill();
+          stroke(255, 0, 0);
+          rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
         }
-    }
+      }
 }
-function modelLoaded(){
-    console.log("Coco has arrived.Bus 165 with SS is running 30 minutes late! D is still asleep and may not arrive at all ! ");
-    status=true;
-    objectDetector.detect('img',gotResult);
+
+  status = true;
+  objectDetector.detect(img, gotResult);
 }
-function gotResult(error,results){
-    if(error){
-        console.error(error);
-    }
-    else{
-        console.log(results);
-        objects=results;
-    }
+
+function gotResult(error, results) {
+  if (error) {
+    console.log(error);
+  }
+  console.log(results);
+  objects = results;
+}
+
+
+function draw() {
+  image(img, 0, 0, 640, 420);
+
+      if(status != "")
+      {
+        for (var i = 0; i < objects.length; i++) {
+          document.getElementById("status").innerHTML = "Status : Object Detected";
+    
+          fill(255, 0, 0);
+          percent = floor(objects[i].confidence * 100);
+          text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15);
+          noFill();
+          stroke(255, 0, 0);
+          rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+        }
+      }
 }
